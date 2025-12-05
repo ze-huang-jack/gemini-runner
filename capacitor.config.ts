b15311888668a28bab157272232bc1f8293db120
ze huang(jack) <suqiuliang727@gmail.com>
2025-12-05 13:32:59 +0800

    上线到安卓

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'gemini-runner',
+  webDir: 'dist'
+};
+
+export default config;
